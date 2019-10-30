dfd7e2ddcf45895ec4aadd6196edd89f5b881555
dyindude <dyindude@github.dyindude.net>
2019-10-30 14:06:21 -0500

    adding some tests for the Add() function

--- main_test.go
+++ main_test.go
@@ -9,3 +9,24 @@ func TestNew(t *testing.T) {
 	empty := New()
 	fmt.Printf("%T\n", empty)
 }
+
+func TestAdd(t *testing.T) {
+	empty := New()
+	one := New()
+	many := New()
+
+	one.Add("foo")
+	many.Add("foo")
+	many.Add("bar")
+
+	if empty.Size != 0 {
+		t.Errorf("empty.Size = %d; want 0", empty.Size)
+	}
+	if one.Size != 1 {
+		t.Errorf("one.Size = %d; want 1", one.Size)
+	}
+	if !many.Size >= 2 {
+		t.Errorf("many.Size = %d; want >= 2", many.Size)
+	}
+
+}
