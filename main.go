01f70eda49ea620a96b40075a056640e84cf1d04
dyindude <dyindude@github.dyindude.net>
2019-10-30 15:35:31 -0500

    implementing Remove() function to pass TestRemove

--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"fmt"
+)
+
 const MAX_SIZE = 10
 
 type Set struct {
@@ -18,6 +22,20 @@ func (s *Set) Add(t string) {
 		s.Items[s.Size] = t
 		s.Size++
 	}
+}
+
+func (s *Set) Remove(t string) error {
+	if !(s.Contains(t)) {
+		return fmt.Errorf("set does not contain string %s", t)
+	}
+	for i, v := range s.Items {
+		if v == t {
+			s.Items[i] = ""
+			s.Size--
+			return nil
+		}
+	}
+	return fmt.Errorf("set did not contain expected value %s after iterating, something is wrong!", t)
 }
 
 func (s *Set) Contains(t string) bool {
